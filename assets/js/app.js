092d4b4b2ac1522713a6a1520ea50d5c1ae78342
CoryKDev <corykeelan7@gmail.com>
2020-12-14 19:59:21 -0800

    created all the javascript with the squad

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const nodemon = require("nodemon");
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+var reservations = [{}];
+
+// Routes
+app.get("/", function(req, res) {
+    res.send("Welcome to the Star Wars Page!");
+  });
+
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+  
+app.post("/api/reservations", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+  
+    // Using a RegEx Pattern to remove spaces fromnewReservation
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+   newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newReservation);
+  
+    reservations.push(newReservation);
+  
+    res.json(newReservation);
+  });
+  
+  // Starts the server to begin listening
+  // =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
